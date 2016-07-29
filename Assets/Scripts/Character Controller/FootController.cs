6463bedb90a3c1e70218b58b50a45097218c4bde
SuperManitu <supermanitu@gmail.com>
2016-07-29 17:40:46 +0200

    rewrote ProbeGround(), does not use foreign code now

--- Assets/Scripts/Character Controller/FootController.cs
+++ Assets/Scripts/Character Controller/FootController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class FootController : MonoBehaviour
 {
     [SerializeField]
-    private float standAngle = 45.0f;
+    private float standAngle = 30.0f;
     [SerializeField]
     private float stepHeight = 0.3f;
 
@@ -50,6 +50,7 @@ public class FootController : MonoBehaviour
 		bool isGrounded = ProbeGround(out hit);
 
         DebugDraw.DrawMarker(hit.point, 0.25f, Color.red, 1);
+		Debug.DrawRay(hit.point, hit.normal, Color.blue, 1);
 
 		if(!lifted)
 		{
@@ -59,26 +60,38 @@ public class FootController : MonoBehaviour
 			}
 			else
 			{
-				PerformGravity();
+				PerformGravity(hit);
 			}
 		}
 
         this.gameObject.layer = 0;
     }
 
-	private void PerformGravity()
+	private void PerformGravity(RaycastHit raycastHit)
 	{
 		currentSpeed += CurrentGravity.gravity * Time.deltaTime;
 
-		RaycastHit hit;
-		if (!Physics.Raycast(Position, GravityDirection, out hit, currentSpeed, layerMask))
+		if(Vector3.Angle(raycastHit.normal, -GravityDirection) > standAngle)
 		{
-			transform.parent.position += GravityDirection * currentSpeed;
+			Vector3 planeNormal = Vector3.Cross(raycastHit.normal, -GravityDirection);
+			Vector3 pDirection = Vector3.Cross(planeNormal, raycastHit.normal);
+			Vector3 nDirection = -pDirection;
+			Vector3 direction = (GravityDirection + pDirection).magnitude < (GravityDirection + nDirection).magnitude ? nDirection : pDirection;
+
+			transform.parent.position += direction.normalized * currentSpeed;
 		}
 		else
 		{
-			transform.parent.position = hit.point + transform.parent.up * ownCollider.radius - transform.localPosition;
-			currentSpeed = 0;
+			RaycastHit hit;
+			if (!Physics.Raycast(Position, GravityDirection, out hit, currentSpeed, layerMask))
+			{
+				transform.parent.position += GravityDirection * currentSpeed;
+			}
+			else
+			{
+				transform.parent.position = hit.point + transform.parent.up * ownCollider.radius - transform.localPosition;
+				currentSpeed = 0;
+			}
 		}
 	}
 
@@ -118,25 +131,26 @@ public class FootController : MonoBehaviour
 
     private bool ProbeGround(out RaycastHit raycastHit)
     {
-        RaycastHit hit;
-
-		if (Physics.Raycast(Position, -transform.parent.up, out hit, stepHeight + tolerance, layerMask))
+		if(Physics.SphereCast(Position, ownCollider.radius, GravityDirection, out raycastHit, currentSpeed + tolerance, layerMask)) //Check if we are colliding at all
 		{
-			if ((hit.point - Position).magnitude > ownCollider.radius + stepHeight + tinyTolerance)
+			if(Vector3.Angle(raycastHit.normal, -GravityDirection) > standAngle) //check if we can stand on the surface we are on
 			{
-				raycastHit = hit;
-				return false;
+				RaycastHit hit;
+				Physics.Raycast(raycastHit.point + raycastHit.normal, raycastHit.point - GravityDirection * tinyTolerance, out hit, 2, layerMask); //Get normal of the wall we are about to slide down
+				Vector3 cross = Vector3.Cross(raycastHit.normal, hit.normal);
+				Vector3 wallDirection = Vector3.Cross(hit.normal, cross);
+				wallDirection = (wallDirection + GravityDirection).magnitude > (-wallDirection + GravityDirection).magnitude ? wallDirection : -wallDirection; //Make sure we point downwards
+
+				RaycastHit floorHit;
+				Physics.Raycast(hit.point + hit.normal * tinyTolerance, wallDirection, out floorHit, Mathf.Infinity, layerMask);
+				if((floorHit.point - raycastHit.point).magnitude > stepHeight)
+				{
+					return false;
+				}
 			}
+			return true;
 		}
-
-		if(Physics.SphereCast(Position, smallerRadius, -transform.up, out hit, stepHeight + tolerance, layerMask))
-		{
-			SimulateSphereCast(hit.normal, out hit);
-		}
-
-		raycastHit = hit;
-
-		return hit.point.y - getY(hit.point) <= stepHeight;
+		return false;
     }
 
     private float getY(Vector3 hit)
@@ -145,44 +159,5 @@ public class FootController : MonoBehaviour
                            - Mathf.Pow(hit.x - Position.x, 2)
                            - Mathf.Pow(hit.z - Position.z, 2)
                 ) + Position.y + ownCollider.center.y;
-    }
-
-    /// <summary>
-    /// Provides raycast data based on where a SphereCast would contact the specified normal
-    /// Raycasting downwards from a point along the controller's bottom sphere, based on the provided
-    /// normal
-    /// </summary>
-    /// <param name="groundNormal">Normal of a triangle assumed to be directly below the controller</param>
-    /// <param name="hit">Simulated SphereCast data</param>
-    /// <returns>True if the raycast is successful</returns>
-    private bool SimulateSphereCast(Vector3 groundNormal, out RaycastHit hit)
-    {
-        float groundAngle = Vector3.Angle(groundNormal, transform.parent.up) * Mathf.Deg2Rad;
-
-        Vector3 secondaryOrigin = Position + transform.parent.up * tolerance;
-
-        if (!Mathf.Approximately(groundAngle, 0))
-        {
-            float horizontal = Mathf.Sin(groundAngle) * ownCollider.radius;
-            float vertical = (1.0f - Mathf.Cos(groundAngle)) * ownCollider.radius;
-
-            // Retrieve a vector pointing up the slope
-            Vector3 r2 = Vector3.Cross(groundNormal, -transform.parent.up);
-            Vector3 v2 = -Vector3.Cross(r2, groundNormal);
-
-            secondaryOrigin += Math3d.ProjectVectorOnPlane(transform.parent.up, v2).normalized * horizontal + transform.parent.up * vertical;
-        }
-
-        if (Physics.Raycast(secondaryOrigin, -transform.parent.up, out hit, Mathf.Infinity, layerMask))
-        {
-            // Remove the tolerance from the distance travelled
-            hit.distance -= tolerance;
-
-            return true;
-        }
-        else
-        {
-            return false;
-        }
     }
 }
